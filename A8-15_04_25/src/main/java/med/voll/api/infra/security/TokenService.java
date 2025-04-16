754d8e0afc4440ac14776308a95eee204b2f9b7d
d4nkali <133923419+d4nkali@users.noreply.github.com>
2025-04-16 08:42:27 -0300

    Validação do Token

--- A8-15_04_25/src/main/java/med/voll/api/infra/security/TokenService.java
+++ A8-15_04_25/src/main/java/med/voll/api/infra/security/TokenService.java
@@ -2,6 +2,7 @@ package med.voll.api.infra.security;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import med.voll.api.domain.usuario.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -31,6 +32,20 @@ public class TokenService {
 			throw new RuntimeException(
 				"Erro ao gerar o token JWT", exception
 			);
+		}
+	}
+
+	public String getSubject(String tokenJWT) {
+		try {
+			var algoritmo = Algorithm.HMAC256(secret);
+			return JWT.require(algoritmo)
+				.withIssuer("API Voll.med")
+				.build()
+				.verify(tokenJWT)
+				.getSubject();
+		} catch (JWTVerificationException exception) {
+			exception.printStackTrace();
+			throw new RuntimeException("Token JWT inválido ou expirado!");
 		}
 	}
 	
